e70717b5964941649767d412902b04add7ee70e8
Jeanette Rosario <jeanetterosario88@gmail.com>
2020-04-11 15:02:30 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
-## code your solution here. 
+class Cat
+
+  def name=(catname)
+    @thedogsname=catname
+  end
+  
+  def name
+    @thecatsname
+  end
+  
+  def bark
+    puts "woof!"
+  end
+
+end
